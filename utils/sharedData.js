1a3afc42ed39e614252602b4ff97318db31b7b89
007codename <007codename@users.noreply.github.com>
2025-09-18 10:33:00 +0200

    b4 rebase

--- utils/sharedData.js
+++
@@ -1,8 +0,0 @@
-// utils/sharedData.js - Shared data storage
-// Simple in-memory storage for tracking how users joined
-// Note: This will reset when bot restarts, but it's a simple solution
-const userJoinSources = new Map();
-
-module.exports = {
-    userJoinSources
-};
